f45b6b49360734b7c1b67f34917b715cad642f40
jorge niglia <jorge.niglia@gmail.com>
2023-06-04 19:32:59 -0300

    server

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,13 @@
 import { Category } from "../Category/index";
 import { List, Item } from "./styles";
+import { categories } from "../../../api/db.json";
+
 export const ListOfCategories = () => {
   return (
     <List>
-      {[1, 2, 3, 4].map((category) => (
-        <Item key={category}>
-          <Category />
+      {categories.map((category) => (
+        <Item key={category.id}>
+          <Category {...category} />
         </Item>
       ))}
     </List>
